591f909f9b101189c46241e665ddf260d6cd3b12
Matt <matt.d.moore@outlook.com>
2022-04-05 14:42:58 +0200

    Implemented page navigation and first version of trial feedback

--- tap2card/Screen.py
+++ tap2card/Screen.py
@@ -1,65 +1,108 @@
-from psychopy import visual, event
+from psychopy import core, visual, event
+from os import listdir
+from re import findall
 
 
 class Screen(visual.Window):
-    def __init__(self, size, backend='pyglet'):
-        super().__init__(size, color=[0., 0., 0.], winType=backend, allowGUI=False)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs, color=[1., 1., 1.], allowGUI=False)
         self.frame_rate = self.getActualFrameRate()
 
+        files = sorted(listdir('instructions'), key=lambda x: int(*findall(r'\d+', x)))
+        self.pages = [visual.ImageStim(self, 'instructions/' + f) for f in files]
+
     def instructions(self):
-        pass
-        # TODO: Replace with Ece's image instructions instead?
-        # task = visual.TextBox2(self,
-        #                        'Task: tap your finger on the drum pad to produce the rhythm you see on the screen')
-        # rules = visual.TextBox2(self,
-        #                         'Please tap according to the following rules:\n'
-        #                         '- Time goes from left to right\n'
-        #                         '- Each box lasts the same amount of time\n'
-        #                         '- Black means tap and white means wait\n'
-        #                         '- Tap in a loop, meaning start from the beginning once you reach the end\n'
-        #                         '   as if the first box follows the last box\n'
-        #                         '- Stop only when the word stop is displayed on the screen\n')
-        # rules.draw()
-        # self.flip()
+        navigation_dict = {'left': -1,
+                           'right': 1,
+                           'return': 0,
+                           'escape': 'quit'}
 
-    def trial_feedback(self, ioi, strength):
-        text = visual.TextBox2(self,
-                               'IOI: {0:.1f}ms\nConfidence: {1:.1f}%'.format(ioi, strength * 100),
-                               pos=(0, .2),
-                               alignment='center')
-        text.draw()
-
-    def uneven_taps(self):
-        text = visual.TextBox2(self,
-                               'IOI: uneven taps\nConfidence: uneven taps',
-                               pos=(0, .2),
-                               alignment='center')
-        text.draw()
-
-    def rhythm_visualiser(self, rhythm, no_flip=False):
-
-        if isinstance(rhythm, list):
-            for i, r in enumerate(rhythm):
-                boxes = self.box_notation(r, i)
-                [b.draw() for b in boxes]
-
-        else:
-            boxes = self.box_notation(rhythm, 0)
-            [b.draw() for b in boxes]
-
-        if not no_flip:
+        i = 0
+        while i < 9:
+            self.pages[i].draw()
             self.flip()
 
-    def box_notation(self, rhythm, i):
-        box_size = .05
-        boxes = [visual.Rect(self,
-                             box_size, box_size,
-                             lineColor=None,
-                             fillColor='black' if r == 1 else 'white',
-                             pos=(x_position(j, box_size, len(rhythm)), i * box_size * 1.1))
-                 for j, r in enumerate(rhythm)]
-        return boxes
+            press = event.waitKeys()[0]
+            if press in navigation_dict.keys():
+                key = navigation_dict[press]
 
+                if key is 'quit':
+                    core.quit()
 
-def x_position(i, box_size, n):
-    return (i * box_size * 1.1) - (n * box_size * 1.1 / 2)
+                elif key == i < 2:  # First 2 pages
+                    i += 1
+
+                elif 1 < i < 8:  # LR navigation
+                    i += key
+
+                elif i == 8:  # Last page
+                    if key < 0:
+                        i += key
+                    elif key == 0:
+                        i += 1
+
+    def show_rhythm(self, trial_num, practice=False):
+        i = 0
+        if trial_num == 0:
+            i = 9 if practice else 13
+        elif trial_num == 1:
+            i = 17 if practice else 18
+        elif trial_num == 2:
+            i = 20 if practice else 21
+
+        # print('show', i)
+        self.pages[i].draw()
+        self.flip()
+        # event.waitKeys()
+
+    def stop(self, practice=False):
+        i = 14 if practice else 19
+        # print('stop', i)
+        self.pages[i].draw()
+        self.flip()
+        # event.waitKeys()
+
+    def finish(self):
+        self.pages[-1].draw()
+        self.flip()
+        # event.waitKeys()
+
+    def uneven_taps(self):
+        self.pages[10].draw()
+        self.flip()
+
+    def incorrect_rhythm(self):
+        self.pages[11].draw()
+        self.flip()
+
+    def incorrect_metre(self):
+        self.pages[12].draw()
+        self.flip()
+
+#     def rhythm_visualiser(self, rhythm, no_flip=False):
+#
+#         if isinstance(rhythm, list):
+#             for i, r in enumerate(rhythm):
+#                 boxes = self.box_notation(r, i)
+#                 [b.draw() for b in boxes]
+#
+#         else:
+#             boxes = self.box_notation(rhythm, 0)
+#             [b.draw() for b in boxes]
+#
+#         if not no_flip:
+#             self.flip()
+#
+#     def box_notation(self, rhythm, i):
+#         box_size = .05
+#         boxes = [visual.Rect(self,
+#                              box_size, box_size,
+#                              lineColor=None,
+#                              fillColor='black' if r == 1 else 'white',
+#                              pos=(x_position(j, box_size, len(rhythm)), i * box_size * 1.1))
+#                  for j, r in enumerate(rhythm)]
+#         return boxes
+#
+#
+# def x_position(i, box_size, n):
+#     return (i * box_size * 1.1) - (n * box_size * 1.1 / 2)
--- tap2card/Trial.py
+++ tap2card/Trial.py
@@ -1,48 +1,111 @@
 from psychopy import event
-import curses
 from tap2card.RhythmFinder import RhythmFinder
 
 
 class Trial:
-    def __init__(self, rhythm):
+    def __init__(self, rhythm, trial_num):
         self.rhythm = rhythm
+        self.trial_num = trial_num
 
     def practice(self, window, drum_pad):
         finder = RhythmFinder(self.rhythm)
-
-        console = curses.initscr()
-        curses.cbreak()
-        curses.curs_set(0)
-
-        window.rhythm_visualiser(self.rhythm)
+        window.show_rhythm(self.trial_num, True)
         drum_pad.listen()
+
         while not event.getKeys():
             if drum_pad.has_intervals and drum_pad.new_tap:
                 drum_pad.new_tap = False
                 intervals = drum_pad.intervals[-len(self.rhythm.durations()) * 3:]
+                history = ' '.join([str(x) if x is not None else '-' for x in intervals])
 
                 rhythm = finder.find_rhythm(intervals)
                 ioi, strength = drum_pad.find_ioi(n=10)
 
-                console.clear()
-                console.addstr(0, 0, 'History: ' + ' '.join([str(x) if x is not None else '-' for x in intervals]))
                 if ioi is None:
-                    console.addstr(1, 0, 'Taps are uneven')
-                else:
-                    console.addstr(1, 0, 'IOI: {0:.1f} Confidence: {1:.2f}%'.format(
-                        ioi, strength * 100))
+                    window.uneven_taps()
 
-                if not rhythm:
-                    console.addstr(2, 0, 'Rhythm does not repeat')
-                else:
-                    console.addstr(2, 0, 'Found: ' + ' '.join([str(x) for x in rhythm.durations()]) +
-                                   ' Match: {}'.format(rhythm == self.rhythm))
-                console.refresh()
+                elif rhythm is not None and sum(rhythm.durations()) != sum(self.rhythm.durations()):
+                    window.incorrect_metre()
 
-        # Clean up
-        console.clear()
-        curses.endwin()
+                elif rhythm != self.rhythm:
+                    window.incorrect_rhythm()
+
+                else:
+                    window.show_rhythm(self.trial_num, True)
+
+                if ioi:
+                    print('IOI:', str(round(ioi, 1)) + 'ms',
+                          '(' + str(round(strength * 100)) + '%)',
+                          'Rhythm:', rhythm.durations() if rhythm is not None else None,
+                          'Correct' if rhythm == self.rhythm else 'Incorrect',
+                          'History:', history)
+                else:
+                    print('IOI:', None,
+                          '(' + str(round(strength * 100)) + '%)',
+                          'Rhythm:', rhythm.durations() if rhythm is not None else None,
+                          'Correct' if rhythm == self.rhythm else 'Incorrect',
+                          'History:', history)
+
+        window.stop(True)
+        event.waitKeys()
+
+        # For debugging
+        # import curses
+        # console = curses.initscr()
+        # curses.cbreak()
+        # curses.curs_set(0)
+        # while not event.getKeys():
+        #     if drum_pad.has_intervals and drum_pad.new_tap:
+        #         drum_pad.new_tap = False
+        #         intervals = drum_pad.intervals[-len(self.rhythm.durations()) * 3:]
+        #
+        #         rhythm = finder.find_rhythm(intervals)
+        #         ioi, strength = drum_pad.find_ioi(n=10)
+        #
+        #         console.clear()
+        #         console.addstr(0, 0, 'History: ' + ' '.join([str(x) if x is not None else '-' for x in intervals]))
+        #         if ioi is None:
+        #             console.addstr(1, 0, 'Taps are uneven')
+        #         else:
+        #             console.addstr(1, 0, 'IOI: {0:.1f} Confidence: {1:.2f}%'.format(
+        #                 ioi, strength * 100))
+        #
+        #         if not rhythm:
+        #             console.addstr(2, 0, 'Rhythm does not repeat')
+        #         else:
+        #             console.addstr(2, 0, 'Found: ' + ' '.join([str(x) for x in rhythm.durations()]) +
+        #                            ' Match: {}'.format(rhythm == self.rhythm))
+        #         console.refresh()
+        # console.clear()
+        # curses.endwin()
+
         drum_pad.reset()
 
     def run(self, window, drum_pad):
-        pass
+        finder = RhythmFinder(self.rhythm)
+        window.show_rhythm(self.trial_num)
+        drum_pad.listen()
+
+        while not event.getKeys():
+            if drum_pad.has_intervals and drum_pad.new_tap:
+                drum_pad.new_tap = False
+                intervals = drum_pad.intervals[-len(self.rhythm.durations()) * 3:]
+                history = ' '.join([str(x) if x is not None else '-' for x in intervals])
+
+                rhythm = finder.find_rhythm(intervals)
+                ioi, strength = drum_pad.find_ioi(n=10)
+
+                if ioi:
+                    print('IOI:', str(round(ioi, 1)) + 'ms',
+                          '(' + str(round(strength * 100)) + '%)',
+                          'Rhythm:', rhythm.durations() if rhythm is not None else None,
+                          'Correct' if rhythm == self.rhythm else 'Incorrect',
+                          'History:', history)
+                else:
+                    print('IOI:', None,
+                          '(' + str(round(strength * 100)) + '%)',
+                          'Rhythm:', rhythm.durations() if rhythm is not None else None,
+                          'Correct' if rhythm == self.rhythm else 'Incorrect',
+                          'History:', history)
+        window.stop()
+        event.waitKeys()
